dc5ed5fe04dcf7d24b9d0561156fb4eb8d65be33
edgzuy <edgzuy@outlook.com>
2025-05-10 22:20:28 -0300

    Update frontend to use deployed backend URL on Render, configure socket.io to communicate with production server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://live-chat-app-ej1t.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
